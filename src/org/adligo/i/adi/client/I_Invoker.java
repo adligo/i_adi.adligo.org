6020ba31b8430b22bdbf23afc4bd9f7af73d5283
scott <scott@adligo.com>
2009-01-30 00:31:29 +0000

    fixing typos

--- src/org/adligo/i/adi/client/I_Invoker.java
+++ src/org/adligo/i/adi/client/I_Invoker.java
@@ -2,14 +2,14 @@ package org.adligo.i.adi.client;
 
 public interface I_Invoker extends I_HandlerAsync {
 	/**
-	 * this should return true if the invoker invokes a procedure on 
+	 * this should return false if the invoker invokes a procedure on 
 	 * a remote machine (the way GWT RCP is tipically used
 	 * 
 	 * This is mostly just a convenience method 
 	 * to help with the adligo gwt_util's RpcProxy setup
 	 * since I am using this to wrap local code as well
 	 * 
-	 * may be usefull for j2me automated setup or 
+	 * may be useful for j2me automated setup or 
 	 * j2se rmi automated setup
 	 * 
 	 * @return
