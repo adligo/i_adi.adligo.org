74fa8e2f860f896c0574c7d4093487d3e9fc0419
scott <scott@adligo.com>
2009-05-01 03:59:26 +0000

    added some new stuff

--- src/org/adligo/i/adi/client/InvokerNames.java
+++ src/org/adligo/i/adi/client/InvokerNames.java
@@ -6,4 +6,13 @@ public class InvokerNames {
 	 */
 	public static final String CACHE_READER = "org.adligo.i.adi.cache_reader";
 	public static final String CACHE_WRITER = "org.adligo.i.adi.cache_writer";
+	/**
+	 * this invoker should provide a Map<String,Object>
+	 * for configuration information about the system
+	 * GWT does not use this since the Configuration 
+	 * must be loaded through async calls (and adi is a synchronus api)
+	 * j2se and j2me systems (subsystems) should use this to obtain
+	 * configuraiton information.
+	 */
+	public static final String CONFIGURATION_PROVIDER = "org.adligo.i.adi.configuration_provider";
 }
