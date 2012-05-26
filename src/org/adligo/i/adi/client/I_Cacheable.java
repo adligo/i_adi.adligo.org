d19d081df1fc3d31aefcb8a3323b6a6d07a54539
scott <scott@adligo.com>
2012-05-26 00:02:12 +0000

    constants

--- src/org/adligo/i/adi/client/I_Cacheable.java
+++ src/org/adligo/i/adi/client/I_Cacheable.java
@@ -1,10 +1,27 @@
 package org.adligo.i.adi.client;
 
 public interface I_Cacheable {
+	public static final int OBJECT = 8;
+	public static final int BOOLEAN = 1;
+	public static final int BYTE = 1;
+	public static final int SHORT = 2;
+	public static final int FLOAT = 4;
+	public static final int DOUBLE = 8;
+	public static final int LONG = 4;
+	
 	/**
 	 * return the estimated number of bytes this instance takes
-	 * can be used to make decisions about memory in the cache api
+	 * can be used to make decisions about memory in the cache api.
+	 * Note this should be a deep calculation, (references should 
+	 * calculate their size)
+	 * 
+	 * Note classes like String and Char depend on their charset
+	 * assume UTF-8 
+	 * String.getBytes().length
+	 * 
 	 * @return
 	 */
 	public int getMemsize();
+	
+	
 }
