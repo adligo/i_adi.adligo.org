dc93dfe1579f30e4ec47c4272cd9b3dbebb44e10
scott <scott@adligo.com>
2012-05-26 00:06:08 +0000

    missed int

--- src/org/adligo/i/adi/client/I_Cacheable.java
+++ src/org/adligo/i/adi/client/I_Cacheable.java
@@ -6,6 +6,7 @@ public interface I_Cacheable {
 	public static final int BYTE = 1;
 	public static final int SHORT = 2;
 	public static final int FLOAT = 4;
+	public static final int INT = 4;
 	public static final int DOUBLE = 8;
 	public static final int LONG = 4;
 	
